5796f0061355359b48df286111d9f8cde50cc4ce
Xibalba <xibalba2minecraft@gmail.com>
2022-07-05 14:48:39 +0200

    Setting up heroku

--- src/main/java/fr/xibalba/meteobot/MeteoBotApplication.java
+++ src/main/java/fr/xibalba/meteobot/MeteoBotApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeteoBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeteoBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        SpringApplication.run(MeteoBotApplication.class, args);
+    }
 
 }
