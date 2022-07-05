21f0b0082339b8dff83d47b19a11aa6b9009b174
Xibalba <xibalba2minecraft@gmail.com>
2022-07-05 14:34:55 +0200

    Initial commit

---
+++ src/main/java/fr/xibalba/meteobot/MeteoBotApplication.java
@@ -0,0 +1,13 @@
+package fr.xibalba.meteobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/xibalba/meteobot/MeteoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.xibalba.meteobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
